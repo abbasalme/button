0c8f94bb60e3cf8a9359eab36f77d3d0f6164a73
abbas alme <abbasalme99@gmail.com>
2023-05-18 11:23:10 -0700

    update

---
+++ combobox.py
@@ -0,0 +1,8 @@
+#import from tkinter library 
+import tkinter as tk
+from tkinter import ttk 
+
+#create function which handles the event 
+def on_select(even):
+
+    selected item = event.widget.get()
