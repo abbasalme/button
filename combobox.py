ef76f4082eac94021d8c86ac5cff960a02d27f21
abbas alme <abbasalme99@gmail.com>
2023-05-25 10:37:15 -0700

    update combobox

--- combobox.py
+++ combobox.py
@@ -2,7 +2,25 @@
 import tkinter as tk
 from tkinter import ttk 
 
-#create function which handles the event 
-def on_select(even):
+#create function which handles the event for the combobox, then after selecting an item print selected item
+def on_select(event):
 
-    selected item = event.widget.get()
+    selected_item = event.widget.get()
+    print("Selected item:", selected_item)
+
+#create root window and title for root window 
+root = tk.Tk()
+root.title("Combobox Example")
+
+#create array item list
+items =["Item 1", "Item 2", "Item 3", "Item 4"]
+
+#create the comboboxobject 
+combo_box = ttk.Combobox(root, values=items)
+
+#bind the selected event with the combobox function 
+combo_box.bind("<<ComboboxSelected>>", on_select)
+combo_box.pack()
+
+root.mainloop()
+
